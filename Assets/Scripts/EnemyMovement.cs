1e8f7ee7e9c8beb42f2b861c7fd00e63bb6ae997
Mert <mertiq@icloud.com>
2020-12-08 19:27:07 +0300

    Enemy AI

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    Transform target;
+    int wavepointIndex = 0;
+
+    private void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+        
+        if(Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWayPoint();
+        }
+    
+    }
+
+    void GetNextWayPoint()
+    {
+        if(wavepointIndex >= Waypoints.points.Length-1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
+    }
+
+}
