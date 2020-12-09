a9bf86f58b668b7dd9e90523ea8b2d8667308ef6
Mert <mertiq@icloud.com>
2020-12-09 11:19:58 +0300

    Wave Spawner

---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public Text waveCountdownText;
+
+    public float timeBetweenWaves = 5.5f;
+
+    float countdown = 2f;
+
+    int waveNumber = 0;
+
+    private void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+        countdown -= Time.deltaTime;
+        waveCountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveNumber++;
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+        
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
