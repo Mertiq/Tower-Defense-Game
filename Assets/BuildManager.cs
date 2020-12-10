d6ce88e14275da9d60e6fc38922af5981ffe711d
Mert <mertiq@icloud.com>
2020-12-10 14:34:08 +0300

    Building

---
+++ Assets/BuildManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.Log("more than one buildmanager");
+            return;
+        }
+        instance = this;
+    }
+
+    public GameObject standartTurrentPrefab;
+
+    private void Start()
+    {
+        turrentToBuild = standartTurrentPrefab;
+    }
+
+    GameObject turrentToBuild;
+
+    public GameObject GetTurrentToBuild()
+    {
+        return turrentToBuild;
+    }
+}
---
+++ Assets/Node.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    Color startColor;
+    Renderer rend;
+    GameObject turret;
+    public Vector3 offset;
+
+    private void Awake()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        if(turret != null)
+        {
+            Debug.Log("Cant build here");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurrentToBuild();
+        turret = Instantiate(turretToBuild, transform.position + offset, transform.rotation);
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
